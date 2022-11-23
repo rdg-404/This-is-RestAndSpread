75c462cf1a5670e17e7627a23f32d27df7e436a3
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-11-22 21:50:15 -0300

    rest example

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+// ================= REST =========================
+
+
+
+// imprime o primeiro normal e o resto joga dentro de um array
+function showList(company, ...clients) {
+    console.log(company)
+    console.log(clients)
+}
+
+
+showList('Origamid','Joao', 'Maria', 10)
