7f43f86725c4e87e4c9b4ac08a1ab5ccd7da7158
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-11-22 21:57:25 -0300

    spread example

--- script.js
+++ script.js
@@ -5,11 +5,23 @@
 
 
 
-// imprime o primeiro normal e o resto joga dentro de um array
-function showList(company, ...clients) {
-    console.log(company)
-    console.log(clients)
-}
+// // imprime o primeiro normal e o resto joga dentro de um array
+// function showList(company, ...clients) {
+//     console.log(company)
+//     console.log(clients)
+// }
 
 
-showList('Origamid','Joao', 'Maria', 10)
+// showList('Origamid','Joao', 'Maria', 10)
+
+
+
+
+
+
+
+// ================= SPREAD =========================
+
+// const numeros = [1, 2, 3, 4, 5, 66, 7, 8, 9]
+
+// console.log(Math.max(...numeros))  ///forma de espalhar os numeros ou por um por um no array
