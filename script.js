67bdc82e1d844a4fe08a5a0d391d0f86c7e11948
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2022-11-22 22:26:31 -0300

    clone lost props from class

--- script.js
+++ script.js
@@ -49,15 +49,51 @@
 
 // - 2 - Clonando objetos
 
-const carro = {
-    cor: "azul",
-    portas: 4,
-    ano: 2022
-};
+// const carro = {
+//     cor: "azul",
+//     portas: 4,
+//     ano: 2022
+// };
 
 
-const cloneCarro = {...carro, turbo: true} //pega todas as props de carro e add mais
+// const cloneCarro = {...carro, turbo: true} //pega todas as props de carro e add mais
 
 
-console.log(cloneCarro2)
+// console.log(cloneCarro2)
+
+
+
+//  - 3 - Clones perdem as funcionalidades dos prototypes
+
+// class Transporte {
+//     constructor() {
+//         this.terrestre = true;
+//     }
+//     andar(){
+//         console.log("andou")
+//     }
+// }
+
+// class Carro extends Transporte {
+//     constructor(cor, portas) {
+//         super(); //ativar a extends de transporte
+//         this.cor = cor;
+//         this.portas = portas;
+//     }
+// }
+
+
+// const carro = new Carro('vermelho', 4); //cria um novo obj apartir da class
+
+// const cloneCarro = {...carro}
+
+// console.log(cloneCarro) //nao herda as props da class
+
+// console.log(carro) //herdar as props da class
+
+
+
+/// RESUMO
+
+// Clones pegam o que ja tem e criam um novo, mas nao pegam as props da class
 
